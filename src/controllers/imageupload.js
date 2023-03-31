b95eee5f25c3f4156529b352f19a57b259dced07
James Sample <jamesgsample@gmail.com>
2023-03-31 07:34:35 +0100

    some refactoring

--- src/controllers/imageupload.js
+++ src/controllers/imageupload.js
@@ -1,37 +1,65 @@
-import AWS from "aws-sdk";
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
 const {
-  REACT_APP_DEFAULT_IMAGE,
   REACT_APP_AWS_S3_ACCESS_KEY_ID,
   REACT_APP_AWS_S3_SECRET_KEY,
   REACT_APP_AWS_S3_REGION,
   REACT_APP_S3_BUCKET_NAME,
 } = process.env;
 
-const imageUpload = () => {
-  AWS.config.update({
-    accessKeyId: `${REACT_APP_AWS_S3_ACCESS_KEY_ID}`,
-    secretAccessKey: `${REACT_APP_AWS_S3_SECRET_KEY}`,
-    region: `${REACT_APP_AWS_S3_REGION}`,
-    signatureVersion: "v4",
-  });
+AWS.config.update({
+  accessKeyId: `${REACT_APP_AWS_S3_ACCESS_KEY_ID}`,
+  secretAccessKey: `${REACT_APP_AWS_S3_SECRET_KEY}`,
+  region: `${REACT_APP_AWS_S3_REGION}`,
+  signatureVersion: "v4",
+});
 
-  const s3 = new AWS.S3();
-
-  const uploadToS3 = async (event) => {
-    if (!file) {
-      return;
-    }
-    const params = {
-      Bucket: `${REACT_APP_S3_BUCKET_NAME}`,
-      Key: `${Date.now()}.${file.name}`,
-      Body: file,
-    };
-    const { Location } = await s3.upload(params).promise();
-    event.preventDefault();
-    setImageUrl(Location);
-    setFields((prevState) => ({ ...prevState, image: Location }));
-    console.log("uploading to s3", Location);
-  };
+const uploadImage = async (req, res) => {
+  // let uploadFile = req.files.file;
+  // const fileName = req.files.file.name;
+  console.log("hi");
+  console.log(req.body);
+  console.log(req.data);
+  // console.log(fileName);
+  // const params = {
+  //   Bucket: `${REACT_APP_S3_BUCKET_NAME}`,
+  //   Key: `${Date.now()}.${file.name}`,
+  //   Body: file,
+  // };
+  // try {
+  //   const { Location } = await s3.upload(params).promise();
+  //   res.send(Location).status(201);
+  // } catch {
+  //   res.sendStatus(501);
+  // }
 };
+module.exports = uploadImage;
 
-export default imageUpload;
+// const imageUpload = () => {
+//   AWS.config.update({
+//     accessKeyId: `${REACT_APP_AWS_S3_ACCESS_KEY_ID}`,
+//     secretAccessKey: `${REACT_APP_AWS_S3_SECRET_KEY}`,
+//     region: `${REACT_APP_AWS_S3_REGION}`,
+//     signatureVersion: "v4",
+//   });
+
+//   const s3 = new AWS.S3();
+
+//   const uploadToS3 = async (event) => {
+//     if (!file) {
+//       return;
+//     }
+//     const params = {
+//       Bucket: `${REACT_APP_S3_BUCKET_NAME}`,
+//       Key: `${Date.now()}.${file.name}`,
+//       Body: file,
+//     };
+//     const { Location } = await s3.upload(params).promise();
+//     event.preventDefault();
+//     setImageUrl(Location);
+//     setFields((prevState) => ({ ...prevState, image: Location }));
+//     console.log("uploading to s3", Location);
+//   };
+// };
+
+// export default imageUpload;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,22 @@ const port = 4000;
 const cors = require("cors");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+const multer = require("multer");
+// const middle = express.urlencoded({ extended: true, limit: 1000000 });
 const insertProperty = require("./controllers/addproperty.js");
 const fetchProperties = require("./controllers/getproperties.js");
+const uploadImage = require("./controllers/imageupload");
+
+const middle = multer({ dest: "files" });
 
 app.get("/", (req, res) => {
   res.send("Hi from the backend");
@@ -15,6 +27,7 @@ app.get("/", (req, res) => {
 
 app.post("/addProperty", insertProperty);
 app.get("/getProperties", fetchProperties);
+app.post("/upload", middle.any(), uploadImage);
 
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
